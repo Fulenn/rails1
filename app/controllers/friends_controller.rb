8a078e673e60b61d3a3d9c9f4f94ca96ddb1f0ce
Sebastian Crasnanic <sebastian.crasnanic@reisbalans.nl>
2022-11-09 10:52:35 +0100

    Adds Show method to friends_controller

--- app/controllers/friends_controller.rb
+++ app/controllers/friends_controller.rb
@@ -2,4 +2,9 @@ class FriendsController < ApplicationController
   def index
     @friends = Friend.all
   end
+
+  # GET /friends/1 or /friends/1.json
+  def show
+    @friends =Friend.find(params[:id])
+  end
 end
